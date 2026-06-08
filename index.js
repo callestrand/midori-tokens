d5974054689b6ce130ac6b9d59e77d8c491d729f
Calle Strand <calle@callestrand.se>
2026-06-08 11:49:55 +0700

    Add up:true to display and headingL styles

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ export const F = {
 }
 
 export const TY = {
-  display:   { font: F.serif, size: "var(--fs-display)",   weight: "var(--fw-light)",  ls: "var(--ls-heading)", lh: "var(--lh-heading)"   },
-  headingL:  { font: F.serif, size: "var(--fs-headingl)",  weight: "var(--fw-light)",  ls: "var(--ls-heading)", lh: "var(--lh-heading)"   },
+  display:   { font: F.serif, size: "var(--fs-display)",   weight: "var(--fw-light)",  ls: "var(--ls-heading)", lh: "var(--lh-heading)",  up: true },
+  headingL:  { font: F.serif, size: "var(--fs-headingl)",  weight: "var(--fw-light)",  ls: "var(--ls-heading)", lh: "var(--lh-heading)",  up: true },
   headingM:  { font: F.serif, size: "var(--fs-headingm)",  weight: "var(--fw-light)",  ls: "var(--ls-heading)", lh: "var(--lh-heading)",  up: true },
   quote:     { font: F.serif, size: "var(--fs-quote)",     weight: "var(--fw-light)",  ls: "var(--ls-heading)", lh: "var(--lh-body)",     up: true },
   headingS:  { font: F.serif, size: "var(--fs-headings)",  weight: "var(--fw-light)",  ls: "var(--ls-heading)", lh: "var(--lh-heading-s)", up: true },
